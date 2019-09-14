9e3eae1ac5c3eef2d2522ad0e0e292eec18b08b7
truthconductor <truthconductor@gmail.com>
2019-09-14 15:27:11 +0900

    Update application.rb
    controller作成時の同時生成ファイルを抑制

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module FreemarketSample54b
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # controller作成時の同時生成ファイルの抑制
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
