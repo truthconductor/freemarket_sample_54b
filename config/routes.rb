9492c0c74455fc1170e4e71f420139911f547231
truthconductor <truthconductor@gmail.com>
2019-09-14 18:23:05 +0900

    マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'mypage', to: 'users#mypage', as: :mypage
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'test#index'
 end
