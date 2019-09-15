307adfebbf251ec9677cc6774eb157638f5c5147
hiro0914 <hirokura.magic@gmail.com>
2019-09-15 19:31:49 +0900

    画像保存先の条件分岐

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  # テスト/開発環境ではローカルストレージへ画像が保存されるように条件分岐
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
