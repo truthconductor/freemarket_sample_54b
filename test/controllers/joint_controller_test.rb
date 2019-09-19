536b7f1a53326dbe6fc5204d1f5d00fa95c89ca2
minatosan <yuuki3yuki3@yahoo.co.jp>
2019-09-20 03:44:44 +0900

    rename controller

--- test/controllers/joint_controller_test.rb
+++ test/controllers/joint_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TestControllerTest < ActionDispatch::IntegrationTest
+class JointControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
