976976b4b375ea5211793f7bc8d20b3373ccbd41
minatosan <yuuki3yuki3@yahoo.co.jp>
2019-09-21 04:12:47 +0900

    rewrite PopularCategoly

--- app/controllers/joint_controller.rb
+++ app/controllers/joint_controller.rb
@@ -3,5 +3,6 @@ class JointController < ApplicationController
   def index
    @item ={name:"メルカリさん",price:"1,500"}
    @categoly="レディース"
+   @categolies=["レディース","メンズ","家電・スマホ・カメラ","おもちゃ・ホビー・グッズ"]
   end
 end
