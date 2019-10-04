759f61ddbe7ccfeeb0c83fd43446554f1194368c
hiro0914 <hirokura.magic@gmail.com>
2019-10-05 00:28:29 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'test#index'
+  root to: 'joint#index'
+  resources :joint, only:[:index]
   resources :items, only:[:new, :create, :edit, :update]
 end
