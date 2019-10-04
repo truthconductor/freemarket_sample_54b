b735c050449a1d7014bf5ed728e0ab8b33a56c64
hiro0914 <hirokura.magic@gmail.com>
2019-10-05 00:35:37 +0900

    itemモデルのルーティング訂正

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'joint#index'
   resources :joint, only:[:index]
-  resources :items, only:[:new, :create, :edit, :update]
+  resources :items, only:[:index, :new, :create, :edit, :update]
 end
