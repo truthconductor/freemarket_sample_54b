151db1ed374852bdfe2a0f9c3eede856577c7d18
truthconductor <truthconductor@gmail.com>
2019-10-05 12:48:23 +0900

    出品商品サンプル画像のファイル名を変更

--- app/controllers/joint_controller.rb
+++ app/controllers/joint_controller.rb
@@ -1,8 +1,8 @@
 class JointController < ApplicationController
 
   def index
-   @item ={name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"}
-   @items={name:"メルカリ",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"},{name:"メルカリさん",price:"1,500",image:"メルカリ画像.jpeg"}
+   @item ={name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"}
+   @items={name:"メルカリ",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"},{name:"メルカリさん",price:"1,500",image:"item-sample.jpeg"}
    @categoly="レディース"
    @categolies=["レディース","メンズ","家電・スマホ・カメラ","おもちゃ・ホビー・グッズ"]
   end
