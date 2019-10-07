232f2a8e7aeec2f89d19bd12cf3737330217aa32
hiro0914 <hirokura.magic@gmail.com>
2019-10-07 23:58:32 +0900

    コンフリクトの解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :joint,only:[:index]
   root to: 'joint#index'
-  resources :joint, only:[:index]
+  get 'mypage', to: 'users#mypage', as: :mypage
+  resources :test,only:[:index]
   resources :items, only:[:index, :new, :create, :edit, :update]
 end
