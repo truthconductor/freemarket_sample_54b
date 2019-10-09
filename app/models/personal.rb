6026a4f7bfa94953ae9239c74a9f9c5bf78e4696
minatosan <yuuki3yuki3@yahoo.co.jp>
2019-10-10 04:12:03 +0900

    personalテーブル　バリデーション追加

--- app/models/personal.rb
+++ app/models/personal.rb
@@ -1,4 +1,18 @@
 class Personal < ApplicationRecord
+  #Validation
+  validates :first_name, length: {maximum: 35}
+  validates :first_name, presence: true
+  validates :last_name, length: {maximum: 35}
+  validates :first_name, presence: true
+  validates :first_name_kana, length: {maximum: 35}
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, length: {maximum: 35}
+  validates :first_name_kana, presence: true
+  validates :zip_code,length: {maximum: 8}
+  validates :city,length: {maximum: 50}
+  validates :address,length: {maximum: 100}
+  validates :building,length: {maximum: 100}
+  validates :cellular_phone_number,length: {maximum: 35}
   belongs_to :user
   #belongs_to_active_hash :prefecture
 end
