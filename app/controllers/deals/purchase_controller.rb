c065204a3c10d16ae2a61c8a25d734e990e9eb20
truthconductor <truthconductor@gmail.com>
2019-10-19 17:28:04 +0900

    データベースの商品情報を商品購入確認ページに反映

--- app/controllers/deals/purchase_controller.rb
+++ app/controllers/deals/purchase_controller.rb
@@ -5,9 +5,13 @@ class Deals::PurchaseController < ApplicationController
 
   def new
     # 購入確認商品を取得する
-    @item = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id]).decorate
     # ログインユーザーのカード情報を取得する
     @credit_card = current_user.credit_card
     @payjp_card = @credit_card&.getPayjpDefaultCard
+  end
+
+  def create
+    @deals = Deal.new(date: Date.today)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
   has_one :personal, dependent: :destroy
-  has_one :deliver_address, dependent: :destroy
+  has_one :delivery_address, dependent: :destroy
   has_many :items, class_name: 'Item', foreign_key: 'seller_id'
   has_many :buyer_deals, class_name: 'Deal', foreign_key: 'buyer_id'
   has_many :seller_deals, class_name: 'Deal', foreign_key: 'seller_id'
