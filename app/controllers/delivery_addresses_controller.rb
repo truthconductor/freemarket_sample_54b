3e006ac8bd957a6ab235da2323d4af3c8fe64c20
truthconductor <truthconductor@gmail.com>
2019-10-19 23:56:13 +0900

    発送先の取得、登録、変更機能追加

--- app/controllers/delivery_addresses_controller.rb
+++ app/controllers/delivery_addresses_controller.rb
@@ -1,7 +1,12 @@
 class DeliveryAddressesController < ApplicationController
   # ログインしていない場合ログインページへ移動する
   before_action :authenticate_user!
+
   def new
     @delivery_address = DeliveryAddress.new
+  end
+
+  def edit
+    @delivery_address = current_user.delivery_address
   end
 end
--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,2 +1,30 @@
 class DeliveryAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  #Validation
+  validates :last_name, presence: true
+  validates :last_name, length: {maximum: 35}
+  validates :first_name, presence: true
+  validates :first_name, length: {maximum: 35}
+  validates :last_name_kana, presence: true
+  validates :last_name_kana, length: {maximum: 35}
+  validates :first_name_kana, presence: true
+  validates :first_name_kana, length: {maximum: 35}
+  validates :zip_code, presence: true
+  validates :zip_code, length: {maximum: 8}
+  validate :check_prefecture
+  validates :city, presence: true
+  validates :city, length: {maximum: 50}
+  validates :address, presence: true
+  validates :address, length: {maximum: 100}
+  validates :building, length: {maximum: 100}
+  validates :phone_number,length: {maximum: 35}
+  #Association
+  belongs_to :user
+  belongs_to_active_hash :prefecture
+
+  def check_prefecture
+    if prefecture_id.nil?
+      errors.add(:prefecture, "選択してください")
+    end
+  end
 end
