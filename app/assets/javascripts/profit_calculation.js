734c7101a1812a581bc8f6065b4a8b2ac9faa6e5
hiro0914 <hirokura.magic@gmail.com>
2019-10-26 20:44:45 +0900

    手数料/利益の計算

---
+++ app/assets/javascripts/profit_calculation.js
@@ -0,0 +1,18 @@
+$(document).on('turbolinks:load', function() {
+  $(document).on('keyup', '.product-value', function(event) {
+    var price = $(this).val();
+
+    if(price >= 300 && price <= 9999999) {
+      var margin = Math.floor(price*0.1);
+      var profit = price - margin;
+      margin = '¥' + String(margin).replace(/(\d)(?=(\d\d\d)+$)/g, '$1,');
+      profit = '¥' + String(profit).replace(/(\d)(?=(\d\d\d)+$)/g, '$1,');
+      $('.margin-calculation__result').html(margin);
+      $('.profit-calculation__result').html(profit);
+
+    } else {
+      $('.margin-calculation__result').html("-");
+      $('.profit-calculation__result').html("-");
+    }
+  });
+});
