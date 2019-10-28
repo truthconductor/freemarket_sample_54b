872cefb0499edd6ff24a3b6cc5549e4f517534b5
minatosan <yuuki3yuki3@yahoo.co.jp>
2019-10-28 18:14:36 +0900

    メール認証設定　修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,18 +93,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-config.action_mailer.default_url_options = { host: '自分のHerokuアプリのドメイン' } #hoge.herokuapp.com
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings =
-{
- user_name: ENV['SENDGRID_USERNAME'],
- password: ENV['SENDGRID_PASSWORD'],
- domain: "heroku.com",
- address: "smtp.sendgrid.net",
- port: 587,
- authentication: :plain,
- enable_starttls_auto: true
-}
+  config.action_mailer.default_url_options = { host: '52.194.143.47' } #hoge.herokuapp.com
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'y',
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
 end
 
 #master keyの指定漏れを防ぐ
