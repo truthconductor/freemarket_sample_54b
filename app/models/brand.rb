add630422554cc8608f2643e11e8803451a7674d
truthconductor <truthconductor@gmail.com>
2019-10-30 01:25:33 +0900

    アソシエーションの記述順を修正

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,7 +2,7 @@ class Brand < ApplicationRecord
   #Validation
   validates :name, :first_letter, presence: true
   #Association
-  has_many :categories, through: :categories_brands
   has_many :categories_brands
+  has_many :categories, through: :categories_brands
   has_many :items
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,8 @@ class Category < ApplicationRecord
   #Validation
   validates :name, presence: true
   #Association
-  has_many :brands, through: :categories_brands
   has_many :categories_brands
+  has_many :brands, through: :categories_brands
   has_many :items
   has_ancestry
 end
