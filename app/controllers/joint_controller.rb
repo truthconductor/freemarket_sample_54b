668befc1dbc158ba23b038745be241db8d30d0dd
hiro0914 <hirokura.magic@gmail.com>
2019-11-03 18:26:16 +0900

    変数名の訂正

--- app/controllers/joint_controller.rb
+++ app/controllers/joint_controller.rb
@@ -1,13 +1,12 @@
 class JointController < ApplicationController
 
   def index
-    @categories = ["レディース","メンズ","家電・スマホ・カメラ","おもちゃ・ホビー・グッズ"]
+    @category_names = ["レディース","メンズ","家電・スマホ・カメラ","おもちゃ・ホビー・グッズ"]
 
     @items = []
-    @categories.each do |category|
-      @category = category
-      @parent_category = Category.find_by(name: @category)
-      @subtree_ids = @parent_category.subtree_ids
+    @category_names.each do |category_name|
+      @category = Category.find_by(name: category_name)
+      @subtree_ids = @category.subtree_ids
       # 上記親カテゴリ配下のカテゴリに属する販売中の商品の内、直近で出品された10件を一覧表示する。
       @items << Item.includes([:item_images]).where(category_id: @subtree_ids, sales_state_id: 1).order("updated_at DESC").limit(10)
     end
