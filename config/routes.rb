517e237f944b63a6fb37d27065497bd443ceb140
truthconductor <truthconductor@gmail.com>
2019-11-03 15:05:07 +0900

    プロフィール表示実装

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :users, only:[:show]
+
   resources :test,only:[:index]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
