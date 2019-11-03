d67af585c92643339e744f71cbb1eb22041eab07
hiro0914 <hirokura.magic@gmail.com>
2019-11-03 17:10:15 +0900

    商品のリンク先設定、販売中の商品のみ一覧表示されるよう訂正

--- app/controllers/joint_controller.rb
+++ app/controllers/joint_controller.rb
@@ -8,7 +8,8 @@ class JointController < ApplicationController
       @category = category
       @parent_category = Category.find_by(name: @category)
       @subtree_ids = @parent_category.subtree_ids
-      @items << Item.includes([:item_images]).where(category_id: @subtree_ids).order("updated_at DESC").limit(10)
+      # 上記親カテゴリ配下のカテゴリに属する販売中の商品の内、直近で出品された10件を一覧表示する。
+      @items << Item.includes([:item_images]).where(category_id: @subtree_ids, sales_state_id: 1).order("updated_at DESC").limit(10)
     end
   end
 end
