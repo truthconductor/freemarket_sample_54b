9718560424bd80ba91a19b8c131bdb8a4eb25a88
truthconductor <truthconductor@gmail.com>
2019-12-14 20:35:28 +0900

    S3設定先を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,9 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  # master keyの指定漏れを防ぐ
+  # master keyが指定されていない状態でサーバ起動を実行しようとするとエラーが発生する
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -107,6 +109,3 @@ Rails.application.configure do
   }
 end
 
-#master keyの指定漏れを防ぐ
-#master keyが指定されていない状態でサーバ起動を実行しようとするとエラーが発生する
-#config.require_master_key = true
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,8 +13,8 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'upload-image3'
+  config.fog_directory  = 'freemarket54b'
   if Rails.env.production?
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-image3'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket54b'
   end
 end
