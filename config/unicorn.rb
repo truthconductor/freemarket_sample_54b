c6a75370d34fddb61fe1b42ec2715a80c4378aeb
truthconductor <truthconductor@gmail.com>
2019-12-14 16:35:16 +0900

    本番環境のフォーク後サーバー切り替え設定
    データベース設定をcredentialsに保存。
    capistranoデプロイ設定を一旦やめ、/var/www/直下で起動できるように変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,26 +1,25 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-# ../が一つ増えている
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
+# アプリケーションサーバの性能を決定する
+# プロセス数の設定（同時アクセスもほぼ無いため分割をしない）
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定
-# currentを指定
-working_directory "#{app_path}/current"
+# Railsアプリケーションの設置されているディレクトリを指定
+working_directory "app_path"
 
 # それぞれ、sharedの中を参照するよう変更
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# Unicornの起動に生成されるpidファイルの設置場所を指定
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# リクエストを受け付けるポート番号を指定
+listen 3000
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
