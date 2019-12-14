7c8102f60f3e93a9652b9aa23bdc5251d991251b
truthconductor <truthconductor@gmail.com>
2019-12-14 17:57:52 +0900

    Nginx経由でリクエストを受け付けるように修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,12 +8,11 @@ worker_processes 1
 # Railsアプリケーションの設置されているディレクトリを指定
 working_directory app_path
 
-# それぞれ、sharedの中を参照するよう変更
 # Unicornの起動に生成されるpidファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 # リクエストを受け付けるポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
